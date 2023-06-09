f665261b812448c99b9d9dc1e7c08359abee640d
Yordii <94014080+Yordii-CE@users.noreply.github.com>
2023-06-09 14:54:59 -0500

    Delete app directory

--- app/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('BASE_URL', 'http://localhost/Carlos/control_inventario');
-define('DEFAULT_PAGE', 'articles');
-
-define('HOST', 'localhost');
-define('DB_NAME', 'control_inventario');
-define('USER', 'root');
-define('PASSWORD', 'Yordii');
-define('CHARSET', 'utf8mb4');
--- app/controllers/error.controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-class ErrorController extends Controller
-{
-
-    function index($message)
-    {
-        $this->view(['message' => $message], false);
-    }
-}
